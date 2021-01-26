f85d415c3982a70421e518edb068eca352e045c6
Kevin Lee <kevinlee@klee-macbook.local>
2021-01-26 09:42:05 -0500

    fix

--- phoneNumber.js
+++ phoneNumber.js
@@ -1,7 +1,9 @@
 $("#checkout-button").click((e) => {
   let phoneNumber = $("#phone-number-input").val();
-  if (phoneNumber.length < 1 && $(".error-container > span").length < 1) {
-    $(".error-container").append(`<span>Please provide valid phone number</span>`)
+  if (phoneNumber.length < 1) {
+    if ($(".error-container > span").length < 1) {
+      $(".error-container").append(`<span>Please provide valid phone number</span>`)
+    }
   } else {
     $(".error-container").empty();
   }
