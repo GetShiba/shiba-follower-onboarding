0096c953d9ae0945ec4a31299b27644a4ab65a8f
Kevin Lee <kevinlee@klee-macbook.local>
2021-01-26 09:36:36 -0500

    phone number screen

--- index.js
+++ index.js
@@ -50,22 +50,6 @@ $(".tier-card-view-perks").click((e) => {
   }
 });
 
-
-var modal = document.getElementById("video-modal");
-var span = document.getElementsByClassName("close")[0];
-
-$(".influencer-video").click((e) => {
-  modal.style.display = "block";
+$("#next-button").click((e) => {
+  window.location.href = 'phoneNumber.html';
 })
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
