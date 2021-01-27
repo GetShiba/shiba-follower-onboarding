e3322b12b3a5871d782643e8f82330629109c4cc
Kevin Lee <kevinlee@klee-macbook.local>
2021-01-26 19:25:15 -0500

    one screen

--- index.js
+++ index.js
@@ -53,3 +53,15 @@ $(".tier-card-view-perks").click((e) => {
 $("#next-button").click((e) => {
   window.location.href = 'phoneNumber.html';
 })
+
+// phone number screen
+$("#checkout-button").click((e) => {
+  let phoneNumber = $("#phone-number-input").val();
+  if (phoneNumber.length < 1) {
+    if ($(".error-container > span").length < 1) {
+      $(".error-container").append(`<span>Please provide valid phone number</span>`)
+    }
+  } else {
+    $(".error-container").empty();
+  }
+})
